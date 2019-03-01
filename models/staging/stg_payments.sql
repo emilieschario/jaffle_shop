655e209baa3ec9fc5b172f2a2279f4d336db8a31
Claire Carroll <carroll.claire.e@gmail.com>
2019-03-01 09:48:01 -0500

    Use references to raw data instead of seeds

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,10 +1,6 @@
 with source as (
-    
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ ref('raw_payments') }}
+
+    select * from raw_jaffle_shop.payments
 
 ),
 
