cd8113bfcf6fe016dd43d8051df757e39df1dc91
Sung Won Chung <sungwon.chung@slalom.com>
2019-11-13 13:56:50 -0600

    remove macro

--- macros/get_custom_schema.sql
+++
@@ -1,7 +0,0 @@
--- put this in macros/get_custom_schema.sql
-
-{% macro generate_schema_name
-(custom_schema_name, node) -%}
-    {{ generate_schema_name_for_env
-(custom_schema_name, node) }}
-{%- endmacro %}
