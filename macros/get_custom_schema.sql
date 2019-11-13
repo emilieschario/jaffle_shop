63f8574a761f1f3d03697eea299df18b32960567
Sung Won Chung <sungwon.chung@slalom.com>
2019-11-13 13:48:23 -0600

    move files up a dir

---
+++ macros/get_custom_schema.sql
@@ -0,0 +1,7 @@
+-- put this in macros/get_custom_schema.sql
+
+{% macro generate_schema_name
+(custom_schema_name, node) -%}
+    {{ generate_schema_name_for_env
+(custom_schema_name, node) }}
+{%- endmacro %}
