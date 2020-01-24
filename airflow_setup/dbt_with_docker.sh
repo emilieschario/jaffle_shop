65a6c4baf43ca0fa27b2cc5029114104cafd1fb9
Sung Won Chung <sung@Sungs-MBP.attlocal.net>
2020-01-24 16:42:22 -0600

    add another argument for ad hoc script

--- airflow_setup/dbt_with_docker.sh
+++ airflow_setup/dbt_with_docker.sh
@@ -1,11 +1,15 @@
+#!/usr/bin/env bash
 
-usage() { echo "Usage: bash $0 [-p DBT_PROFILE_PATH]| \
-Example: bash $0 -p /Users/sung/Desktop/repos/dbt_bigquery_example/profiles.yml " 1>&2; exit 1;}
+usage() { echo "Usage: bash $0 [-p DBT_PROFILE_PATH] [-c DBT_COMMAND] must be run within the directory containing dbt_project.yml file | \
+Example: bash $0 -p /Users/sung/Desktop/repos/dbt_bigquery_example/profiles.yml -c run" 1>&2; exit 1;}
 
-while getopts ":e:u:p:s:g:b:" o; do
+while getopts ":p:c:" o; do
     case "${o}" in
         p)
             p=${OPTARG}
+        ;;
+        c)
+            c=${OPTARG}
         ;;
         *)
             usage
@@ -14,13 +18,15 @@ while getopts ":e:u:p:s:g:b:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${p}" ]; then
+if [ -z "${p}" ] || [ -z "${c}" ]; then
     usage
 fi
 
 # set command line arguments
 DBT_PROFILE_PATH=${p}
+DBT_COMMAND=${c}
 echo "DBT_PROFILE_PATH = $DBT_PROFILE_PATH"
+echo "DBT_COMMAND = dbt $DBT_COMMAND"
 
 # run
 docker run --rm -it \
@@ -36,6 +42,6 @@ dbt_docker:latest dbt debug
 docker run --rm -it \
 -v $PWD:/dbt \
 -v $DBT_PROFILE_PATH:/root/.dbt/profiles.yml \
-dbt_docker:latest dbt run
+dbt_docker:latest dbt $DBT_COMMAND
 
 # /home/realsww123/dbt_bigquery_example/profiles.yml
