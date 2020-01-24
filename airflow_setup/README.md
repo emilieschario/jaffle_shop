96aebaea0db16191962fbfaf0467928887f90847
Sung Won Chung <sung@Sungs-MBP.attlocal.net>
2020-01-24 16:56:37 -0600

    some scripts to setup airflow

---
+++ airflow_setup/README.md
@@ -0,0 +1,20 @@
+# airflow_setup
+
+## What does this do?
+
+- basic airflow setup on your LOCAL machine
+
+## What does this NOT do?
+
+## How to run this?
+
+```bash
+cd <airflow_setup directory>
+
+# setup airflow environment
+bash ./initial_setup.sh
+
+#build image locally
+docker build -t dbt_docker .
+
+```
---
+++ airflow_setup/initial_setup.sh
@@ -0,0 +1,26 @@
+echo "***********************"
+echo "Setup python virtual environment within already created airflow sub-directory"
+sudo easy_install pip
+sudo pip install virtualenv
+virtualenv -p python3 py37_venv
+source py37_venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt --no-cache-dir
+echo "***********************"
+
+echo "***********************"
+echo "Setup airflow directory environment variable"
+echo "***********************"
+export AIRFLOW_HOME="$(pwd)"
+
+echo "***********************"
+echo "Setup airflow configs and list dags"
+echo "***********************"
+airflow initdb
+
+airflow list_dags
+
+echo "***********************"
+echo "Add custom GCP connection using service account key"
+echo "***********************"
+airflow run add_gcp_connection add_gcp_connection_python 2001-01-01
